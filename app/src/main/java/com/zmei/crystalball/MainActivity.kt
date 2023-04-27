a2e3a31bac5ca69042344ccc1fc07ad6af3c4b55
Arhizmei <arhizmei@gmail.com>
2023-04-27 10:48:57 +0300

    home2

--- app/src/main/java/com/zmei/crystalball/MainActivity.kt
+++ app/src/main/java/com/zmei/crystalball/MainActivity.kt
@@ -2,10 +2,23 @@ package com.zmei.crystalball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmei.crystalball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.randomText.setOnClickListener {
+            binding.randomText.text = getReto()
+        }
     }
+private fun getReto(): String{
+    return resources.getStringArray(R.array.retos)[randomNumber()]
+}
+private fun randomNumber(): Int{
+    val size = resources.getStringArray(R.array.retos).size-1
+    return (0..size).random()
+}
 }
